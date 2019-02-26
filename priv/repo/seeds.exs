39d3ceb1e7c950ba38c0f63b06354d436fa86325
Scott Hudson <scott.w.hudson@gmail.com>
2019-02-26 14:31:29 -0600

    more seed stuff

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,16 +11,39 @@
 # and so on) as they will fail if something goes wrong.
 
 defmodule TechThursday.Processor do
-  def process({:ok, _foo}) do
-    IO.puts "ok!"
+  alias TechThursday.{Repo, Inspection}
+  require IEx
+
+  def process({:ok, row}) do
+    Repo.insert(
+      %Inspection{
+        address: row["Address"],
+        facility_id: String.to_integer(row["Facility ID"]),
+        inspection_date: parse_date(row["Inspection Date"]),
+        description: row["Process Description"],
+        restaurant_name: row["Restaurant Name"],
+        score: row["Score"],
+        zip_code: row["Zip Code"]
+      }
+    )
   end
 
   def process({:error, _foo}) do
     IO.puts "Error!"
   end
+
+  def parse_date(string) do
+    IEx.pry
+    m, d, y = String.split(string)
+    Date.new(String.to_integer(y), String.to_integer(m), String.to_integer(d))
+  end
 end
 
-file = File.stream!("./priv/repo/demo_data.csv") |> CSV.decode(headers: true)
+
+file = File.stream!("./priv/repo/demo_data.csv")
+  |> CSV.decode(headers: true)
+
+
 
 
 Enum.each(file, fn(x) -> TechThursday.Processor.process(x) end)
